b99f1a1088d2687dd8ce31858cb5ff3128b821ed
cybercowboy7 <akraft241@gmail.com>
2025-12-01 20:04:12 -0500

    Delete Python_Practice directory

--- Python_Practice/break_statement.py
+++
@@ -1,7 +0,0 @@
-while True:
-	string = input("Enter some text to print.\nType 'Done' when finished: ")
-	if string == "Done":
-		break
-	print(string)
-print("Done!")
-
--- Python_Practice/defining_classes.py
+++
@@ -1,8 +0,0 @@
-class Router:
-	'''Router Class'''  #Doc string to document what the class is for 
-	def __init__(self, model, swversion, ip_add):
-		'''initialize values'''
-		self.model = model
-		self.swversion = swversion
-		self.ip_add = ip_add
-rtr1 = Router("iosV", "15.6.7", "10.1.1.1")
