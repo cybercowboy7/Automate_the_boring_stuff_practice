7e8724eaf11f1ba4a5eaa5216af45a694c5ad761
arkraft <akraft241@gmail.com>
2025-12-04 15:26:01 -0500

    Dictionaries and practice

--- Boring_Stuff_Practice/Dictionary_Initial.py
+++ Boring_Stuff_Practice/Dictionary_Initial.py
@@ -15,20 +15,39 @@
 # print(car_dict == card_dict2)       # For dictionaries, they do not have to have the same order to match
 
 
-# You can iterate through different dictionary items
-spam = {'color': 'red', 'age': 42}
-for k in spam.keys():
-    print(k)
-for v in spam.values():
-    print(v)
-for i in spam.items():          # Key value pair in a dictionary is known as an item
-    print(i)
+# # You can iterate through different dictionary items
+# spam = {'color': 'red', 'age': 42}
+# for k in spam.keys():
+#     print(k)
+# for v in spam.values():
+#     print(v)
+# for i in spam.items():          # Key value pair in a dictionary is known as an item
+#     print(i)
 
 # Can use in and not in operators to check for the presence of keys and values
-print('color' in spam.keys())
-print('black' in spam.values())
+# print('color' in spam.keys())
+# print('black' in spam.values())
 
 
 # If you want to get an actual list from one of these methods, pass its list-like return value to the list() function
-spam_keys_list = list(spam.keys())
-print(*spam_keys_list)
+# spam_keys_list = list(spam.keys())
+# print(*spam_keys_list)
+
+
+# Can utilize multiple assignment trick for dictionaries as well]
+spam = {'color': 'red', 'age': 42}
+for k, v in spam.items():
+    print('Key: ' + str(k) + ' Value: ' + str(v))
+
+# You can retrieve values of keys using the get() function. get(key, fallback value to return if not there)
+print(str(spam.get('name', 0)))     # Returns 0 since 'name' is not a key in the dictionary
+print(str(spam.get('color', 0)))    # Returns red since color is an actual key in the dictionary
+
+# Multiple ways to check if a key is present and add if not present
+if 'name' not in spam:          # Utilizes an if conditional to check if 'name' key is present
+    spam['name'] = 'Bob'
+print(str(spam))
+# OR
+spam.setdefault('Location', 'USA')      # utilizes the setdefault method with the 'Location' key as searching for and the value as 'USA' if the key is not present
+print(str(spam))
+
---
+++ Boring_Stuff_Practice/practice_questions.py
@@ -0,0 +1,41 @@
+# Create variables to store your name, age, and favorite food. Print them in a sentence.
+# name = 'Alex'
+# age = 26
+# favorite_food = 'Italian'
+# print('My name is ' + name + ' and I am ' + str(age) + ' years old. My favorite food is ' + favorite_food)
+
+# Convert a string "25" into an integer, then add 5 to it.
+# def convert_string(string):
+#     integer_value = int(string)
+#     sum = integer_value + 5
+#     print(sum)
+#
+# convert_string('25')
+
+
+# Ask the user for input, then print the data type using type().
+# print('Enter some data:')
+# user_input = input()
+#
+# if user_input.isdigit():
+#     user_input = int(user_input)
+#
+# else:
+#     try:
+#         user_input = float(user_input)
+#     except ValueError:
+#         pass
+#
+# if user_input == 'True':
+#     user_input = True
+# elif user_input == 'False':
+#     user_input = False
+#
+# print(type(user_input))
+
+
+
+
+
+
+# You are writing a program that asks the user how many network devices they manage, stores the value, and prints the total number + 10 new devices being added.
