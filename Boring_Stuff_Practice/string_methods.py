18dd4e013c90ab6b93f4c0817133cdfcfc2692a3
arkraft <akraft241@gmail.com>
2025-12-09 20:39:18 -0500

    String string_methods

--- Boring_Stuff_Practice/string_methods.py
+++ Boring_Stuff_Practice/string_methods.py
@@ -8,6 +8,14 @@
 # print(user_lower)
 
 # isupper() and islower()
-name = 'alex'
-print(name.islower())
-print(name.isupper())
+name = "alex"
+print(name.islower())   # Will print True
+print(name.isupper())   # Will print False
+
+
+# isalpha() Returns True if the string consists only of letters and isn’t blank
+# isalnum() Returns True if the string consists only of letters and numbers (alphanumerics) and isn’t blank
+# isdecimal() Returns True if the string consists only of numeric characters and isn’t blank
+# isspace() Returns True if the string consists only of spaces, tabs, and newlines and isn’t blank
+# istitle() Returns True if the string consists only of words that begin with an uppercase letter followed by only lowercase letters
+
