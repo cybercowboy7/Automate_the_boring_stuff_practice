204f6ad6a45806eb1075b1d0812c5836bd992cce
arkraft <akraft241@gmail.com>
2025-12-09 20:53:10 -0500

    Numeric Code Expl

---
+++ Boring_Stuff_Practice/Numeric_Code_Points.py
@@ -0,0 +1,5 @@
+# every text character has a corresponding numeric value called a Unicode code point.
+# You can use the ord() function to get the code point of a one-character string, and the chr() function to get the one-character string of an integer code point
+
+print(ord('A'))     # prints 65
+print(chr(65))      # prints 'A'
