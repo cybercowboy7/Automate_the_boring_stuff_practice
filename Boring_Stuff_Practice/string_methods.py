9b412c4ca9c5528bce57576ad39c7de28892e341
arkraft <akraft241@gmail.com>
2025-12-09 20:50:56 -0500

    string methods update

--- Boring_Stuff_Practice/string_methods.py
+++ Boring_Stuff_Practice/string_methods.py
@@ -18,4 +18,17 @@ print(name.isupper())   # Will print False
 # isdecimal() Returns True if the string consists only of numeric characters and isn’t blank
 # isspace() Returns True if the string consists only of spaces, tabs, and newlines and isn’t blank
 # istitle() Returns True if the string consists only of words that begin with an uppercase letter followed by only lowercase letters
+# The startswith() and endswith() methods return True if the string value on which they’re called begins or ends (respectively) with the string passed to the method
+# The join() method is useful when you have a list of strings that need to be joined together into a single string value
+# The split() method works the opposite way: we call it on a string value, and it returns a list of strings
+# The rjust() and ljust() string methods return a padded version of the string on which they’re called, with spaces inserted to justify the text. The first argument to both methods is an integer length for the justified string
+# The center() string method works like ljust() and rjust() but centers the text, rather than justifying it to the left or right
+# The strip() string method will return a new string without any whitespace characters at the beginning or end
+# while the lstrip() and rstrip() methods will remove whitespace characters from the left and right ends, respectively
+
+
+
+hello = "Hello"
+print(hello.center(20, '='))
+
 
